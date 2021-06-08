128c801939a5d0d9381a539d97982fc1dc0d6d71
Mari26 <mari.qubro@gmail.com>
2021-06-09 00:51:14 +0400

    inserts

---
+++ davaleba-2.1.sql
@@ -0,0 +1,21 @@
+INSERT INTO  drinks(drinkID,drinkname,drinkcode,price,productiontime,productionperiod )
+VALUES ('12345','asd','1','1.20','1.02.2019','1.02.2019-1.03.2022');
+select * from drinks;
+INSERT INTO  meats(meatID,meatname,meatcode,price,productiontime,productionperiod )
+VALUES ('22222','asdd','1','1.20','1.02.2019','1.02.2019-1.03.2022');
+select * from meats;
+INSERT INTO  meats(meatID,meatname,meatcode,price,productiontime,productionperiod )
+VALUES ('22222','asdd','1','1.20','1.02.2019','1.02.2019-1.03.2022');
+select * from meats;
+INSERT INTO  breads(breadID,breadname,breadcode,price,productiontime,productionperiod )
+VALUES ('88888','dd','1','1.20','1.02.2019','1.02.2019-1.03.2022');
+select * from breads;
+INSERT INTO  provider_sno(provider_snoid,providername,drinkid)
+VALUES ('3','sno');
+select * from provider_sno;
+INSERT INTO  provider_koda(provider_kodaid,providername,meatid)
+VALUES ('4','koda');
+select * from provider_koda;
+INSERT INTO  consultant(consultantid,consultantname)
+VALUES ('1','aaaa');
+select * from consultant;
